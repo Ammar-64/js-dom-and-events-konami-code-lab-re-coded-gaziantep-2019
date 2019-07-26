6a05b8596e5fa7db76c9f7c5d32feeeaac45b9b9
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-26 13:20:22 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -5,20 +5,21 @@ function init() {
 
 
   // This is the function that would be invoked by the event listener.
-  document.body.addEventListener('keydown',function onKeyDownHandler(e) {
+  function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
     let index = 0;
-
+    
     if (key === code[index]) {
       index++;
 
       if (index === code.length) {
         alert("Hurray!");
 
-      
+        index = 0;
       }
     } else {
       index = 0;
     }
-  })
+  }
+  document.body.addEventListener('keydown', onKeyDownHandler);
 }
